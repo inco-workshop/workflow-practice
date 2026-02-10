25aef4cbc4cf1f03aec46dab928468fe87548c16
shpark-inco <shpark@insilicogen.com>
2026-02-10 14:35:45 +0900

    nextflow self-update 제거

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,8 +7,7 @@ export PS1='\[\e[3;36m\]${PWD#/workspaces/} ->\[\e[0m\] '
 # Install vim
 apt-get update && apt-get install -y vim && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-# Update Nextflow
-nextflow self-update
+# Install Nextflow at the version specified by NXF_VER (do NOT use self-update as it overrides NXF_VER)
 nextflow -version
 
 cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt
