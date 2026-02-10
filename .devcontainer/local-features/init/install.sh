906987773190340a8f7f73d44d4080a48828a8fc
shpark-inco <shpark@insilicogen.com>
2026-02-10 15:18:55 +0900

    upload

--- .devcontainer/local-features/init/install.sh
+++ .devcontainer/local-features/init/install.sh
@@ -4,7 +4,7 @@
 
 # Set up directories
 mkdir -p /workspaces/.nextflow
-mkdir -p /workspaces/workflow-practice/
+mkdir -p /workspaces/training/
 
 # Copy over the welcome message
 cp welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
--- .devcontainer/ttt/local-features/uv-tools/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Install python cli tools using uv
-
-uv tool install pre-commit
-uv tool install nf-core
